75f31f3ef2b0b2f1ad0a4388e636eb36d0c95abc
Mehak <mehaktawakley@gmail.com>
2018-08-29 12:46:52 +0530

    init commit

--- setting.py
+++ setting.py
@@ -128,4 +128,4 @@ def result_category():
       return render_template("index.html",result = inp)
 
 if __name__=="__main__":
-	app.run(debug=True,host='0.0.0.0',port=5000)
+	app.run(debug=True,port=4000)
